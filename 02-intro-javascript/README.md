ec675292e69b6d505d3a8e17316a0a36c20e2f0f
joseespiritu <joseespiritu89@aragon.unam.mx>
2021-04-25 19:52:31 -0500

    Introduccion JS const y let

---
+++ 02-intro-javascript/README.md
@@ -0,0 +1,8 @@
+# Bases de JavaScript
+Aplicación de ReactJS para repasar temas de javascript
+
+### Comandos:
+* Inicializar proyecto: _npx create-react-app 02-intro-javascript_
+* Ejecutar proyecto:
+  * _cd 02-intro-javascript_
+  * _npm start_ o _yarn start_
---
+++ 02-intro-javascript/src/bases/01-const-let.js
@@ -0,0 +1,20 @@
+
+// Variables y constantes
+
+const nombre = 'Jose';
+const apellido = 'Espíritu';
+
+let valorDado = 5;
+valorDado = 4;
+
+console.log(nombre, apellido, valorDado);
+
+// var No se usa
+
+if ( true ){
+    const nombre = 'Peter';
+
+    console.log(nombre);
+}
+
+console.log(valorDado);
