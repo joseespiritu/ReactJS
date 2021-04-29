e89777c74419d2befdae2fa41c84082bb9337b11
joseespiritu <joseespiritu89@aragon.unam.mx>
2021-04-28 22:51:15 -0500

    exports

--- 02-intro-javascript/src/data/heroes.js
+++ 02-intro-javascript/src/data/heroes.js
@@ -1,4 +1,4 @@
-export const heroes = [
+const heroes = [
     {
         id: 1,
         name: 'Batman',
@@ -25,3 +25,15 @@ export const heroes = [
         owner: 'Marvel'
     },
 ];
+
+// Manera comun de realizar exportaciones
+/* export const owners = ['Marvel', 'Dc'];
+export default heroes; */
+
+// Otra manera de realizar las exportaciones
+const owners = ['Marvel', 'Dc'];
+
+export {
+    heroes as default,
+    owners
+}
