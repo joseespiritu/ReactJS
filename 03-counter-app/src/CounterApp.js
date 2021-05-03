663101bf403576e6b6899368efdcade793394243
joseespiritu <joseespiritu89@aragon.unam.mx>
2021-05-03 17:24:37 -0500

    useState

--- 03-counter-app/src/CounterApp.js
+++ 03-counter-app/src/CounterApp.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 const CounterApp = ({value}) => {
+
+    const [counter, setCounter] = useState(0);
+
+    // handleAdd
+    const handleAdd = (c) => {
+        //setCounter( counter + 1);
+        setCounter( (c) => c+1 );
+    }
+
     return (
         <>
             <h1>Counter App</h1>
-            <h2>{value}</h2>
+            <h2>{counter}</h2>
+
+            <button onClick={handleAdd} >+1</button>
         </>
     );
 }
