3cc9140fa1a3c270a44057d7119d7fd2b54f28e3
joseespiritu <joseespiritu89@aragon.unam.mx>
2022-03-11 23:49:44 -0600

    Enzyme Testing unit

--- 03-counter-app/src/tests/PrimeraApp.test.js
+++ 03-counter-app/src/tests/PrimeraApp.test.js
@@ -1,12 +1,18 @@
 import React from "react";
-import { render } from "@testing-library/react";
+/* import { render } from "@testing-library/react"; */
 import PrimeraApp from "../PrimeraApp";
+import { shallow } from "enzyme";
 
 describe("Pruebas en <PrimeraApp/>", () => {
-  test('debe mostrar el mensaje "Hola, Mundo"', () => {
+  /* test('debe mostrar el mensaje "Hola, Mundo"', () => {
     const saludo = "Hola, Mundo";
     const { getByText } = render(<PrimeraApp mensaje={saludo} />);
-
     expect(getByText(saludo)).toBeInTheDocument();
+  }); */
+  test("Debe de mostrar <PrimeraApp/> correctamente", () => {
+    const saludo = "Hola, Mundo";
+    const wrapper = shallow(<PrimeraApp mensaje={saludo} />);
+
+    expect(wrapper).toMatchSnapshot();
   });
 });
