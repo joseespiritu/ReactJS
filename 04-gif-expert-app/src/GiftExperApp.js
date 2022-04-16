32e1bd9690870af51743cfb42e64e92ee4a3c2ea
joseespiritu <joseespiritu89@aragon.unam.mx>
2022-04-15 21:46:36 -0500

    gif expert app

---
+++ 04-gif-expert-app/src/GiftExperApp.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import AddCategory from "./components/AddCategory";
+import GifGrid from "./components/GifGrid";
+
+const GiftExperApp = () => {
+  const [categories, setCategories] = useState(["One Punch"]);
+
+  // const handleAdd = () => {
+  //   setCategories([...categories, "HunterXHunter"]);
+  // };
+
+  return (
+    <div>
+      <h2>GiftExperApp</h2>
+      <AddCategory setCategories={setCategories} />
+
+      <hr />
+
+      <ol>
+        {categories.map((category) => (
+          <GifGrid key={category} category={category} />
+        ))}
+      </ol>
+    </div>
+  );
+};
+
+export default GiftExperApp;
---
+++ 04-gif-expert-app/src/components/AddCategory.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
+const AddCategory = ({ setCategories }) => {
+  const [inputValue, setInputValue] = useState("");
+
+  const handdleInputChange = (e) => {
+    // console.log(e.target.value);
+    setInputValue(e.target.value);
+  };
+
+  const hanldeSubmit = (e) => {
+    e.preventDefault();
+
+    if (inputValue.trim().length > 2) {
+      setCategories((cat) => [inputValue, ...cat]);
+      setInputValue("");
+    }
+  };
+
+  return (
+    <form onSubmit={hanldeSubmit}>
+      <input type="text" value={inputValue} onChange={handdleInputChange} />
+    </form>
+  );
+};
+
+AddCategory.propTypes = {
+  setCategories: PropTypes.func.isRequired,
+};
+
+export default AddCategory;
