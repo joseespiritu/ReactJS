615a3d8e8e8ab84a0278e95e02b534b7106f4bbc
joseespiritu <joseespiritu89@aragon.unam.mx>
2022-04-25 07:53:07 -0500

    Formulario custom hook

---
+++ 05-hooks-app/src/components/02-useEffect/FormWithCustomHook.js
@@ -0,0 +1,70 @@
+import React, { useEffect } from "react";
+import "./effects.css";
+import { useForm } from "./useForm";
+
+const FormWithCustomHook = () => {
+  const { values, handleInputChange } = useForm({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const { name, email, password } = values;
+
+  useEffect(() => {
+    console.log("Email cambio");
+  }, [email]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(values);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h1>FormWithCustomHook</h1>
+      <hr />
+
+      <div className="from-group">
+        <input
+          type="text"
+          name="name"
+          className="form-control"
+          placeholder="Tu nombre"
+          autoComplete="off"
+          value={name}
+          onChange={handleInputChange}
+        />
+      </div>
+
+      <div className="from-group">
+        <input
+          type="text"
+          name="email"
+          className="form-control"
+          placeholder="email@gmail.com"
+          autoComplete="off"
+          value={email}
+          onChange={handleInputChange}
+        />
+      </div>
+
+      <div className="from-group">
+        <input
+          type="password"
+          name="password"
+          className="form-control"
+          placeholder="*******"
+          value={password}
+          onChange={handleInputChange}
+        />
+      </div>
+
+      <button type="submit" className="btn btn-primary">
+        Guardar
+      </button>
+    </form>
+  );
+};
+
+export default FormWithCustomHook;
--- 05-hooks-app/src/index.js
+++ 05-hooks-app/src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import FormWithCustomHook from "./components/02-useEffect/FormWithCustomHook";
 // import CounterApp from "./components/01-useState/CounterApp";
 // import CounterWithCustomHook from "./components/01-useState/CounterWithCustomHook";
-import SimpleForm from "./components/02-useEffect/SimpleForm";
+// import SimpleForm from "./components/02-useEffect/SimpleForm";
 // import HookApp from "./HookApp";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<SimpleForm />);
+root.render(<FormWithCustomHook />);
