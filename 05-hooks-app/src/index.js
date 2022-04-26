444f85ede327bc42b50f7edda5c53173f6a0b6bf
joseespiritu <joseespiritu89@aragon.unam.mx>
2022-04-25 21:19:21 -0500

    useFecth customHook

--- 05-hooks-app/src/index.js
+++ 05-hooks-app/src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import FormWithCustomHook from "./components/02-useEffect/FormWithCustomHook";
+import MultipleCustomHooks from "./components/03-examples/MultipleCustomHooks";
+// import FormWithCustomHook from "./components/02-useEffect/FormWithCustomHook";
 // import CounterApp from "./components/01-useState/CounterApp";
 // import CounterWithCustomHook from "./components/01-useState/CounterWithCustomHook";
 // import SimpleForm from "./components/02-useEffect/SimpleForm";
 // import HookApp from "./HookApp";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<FormWithCustomHook />);
+root.render(<MultipleCustomHooks />);
