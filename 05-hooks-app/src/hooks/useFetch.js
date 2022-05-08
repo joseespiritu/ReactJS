99da598eeb9f267be0643861793a8eaf8aa475fc
joseespiritu <joseespiritu89@aragon.unam.mx>
2022-05-08 13:51:17 -0500

    pruebas useFetch

--- 05-hooks-app/src/hooks/useFetch.js
+++ 05-hooks-app/src/hooks/useFetch.js
@@ -28,6 +28,13 @@ export const useFetch = (url) => {
         } else {
           console.log("setState no se llamo");
         }
+      })
+      .catch(() => {
+        setState({
+          data: null,
+          loading: false,
+          error: "No se pudo cargar la info",
+        });
       });
   }, [url]);
 
