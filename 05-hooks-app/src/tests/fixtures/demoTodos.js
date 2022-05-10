e822884b6a0420e155242ffdcbd21816c3f48889
joseespiritu <joseespiritu89@aragon.unam.mx>
2022-05-09 22:37:42 -0500

    pruebas todoReducer

---
+++ 05-hooks-app/src/tests/fixtures/demoTodos.js
@@ -0,0 +1,12 @@
+export const demoTodos = [
+  {
+    id: 1,
+    description: "Aprender react",
+    done: false,
+  },
+  {
+    id: 2,
+    description: "Aprender mongo",
+    done: false,
+  },
+];
