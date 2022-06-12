9fb108bdb93c2438e225eece2198074a8d2017b8
joseespiritu <joseespiritu89@aragon.unam.mx>
2022-06-12 17:08:10 -0500

    Listado de heroes

--- 07-heroes-app/src/components/marvel/MarvelScreen.js
+++ 07-heroes-app/src/components/marvel/MarvelScreen.js
@@ -1,7 +1,11 @@
+import { HeroList } from "../hero/HeroList";
+
 export const MarvelScreen = () => {
   return (
-    <div>
+    <div className="container mt-5">
       <h1>MarvelScreen</h1>
+      <hr />
+      <HeroList publisher={"Marvel Comics"} />
     </div>
   );
 };
