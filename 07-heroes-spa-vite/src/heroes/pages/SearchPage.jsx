e5050f426936ac3e5062f981b3bd932f7c39b204
joseespiritu <joseespiritu89@aragon.unam.mx>
2022-06-19 21:52:28 -0500

    Listado de heroes, mensajes

--- 07-heroes-spa-vite/src/heroes/pages/SearchPage.jsx
+++ 07-heroes-spa-vite/src/heroes/pages/SearchPage.jsx
@@ -2,20 +2,25 @@ import { useLocation, useNavigate } from "react-router-dom";
 import queryString from "query-string";
 import { useForm } from "../../hooks/useForm";
 import { HeroCard } from "../components";
+import { getHeroesByName } from "../helpers";
 
 export const SearchPage = () => {
   const navigate = useNavigate();
   const location = useLocation();
 
   const { q = "" } = queryString.parse(location.search);
+  const heroes = getHeroesByName(q);
+
+  const showSearch = q.length === 0;
+  const showError = q.length > 0 && heroes.length === 0;
 
   const { searchText, onInputChange } = useForm({
-    searchText: "",
+    searchText: q,
   });
 
   const onSearchSubmit = (event) => {
     event.preventDefault();
-    if (searchText.trim().length <= 1) return;
+    // if (searchText.trim().length <= 1) return;
     navigate(`?q=${searchText}`);
   };
 
@@ -45,12 +50,23 @@ export const SearchPage = () => {
           <h4>Results</h4>
           <hr />
 
-          <div className="alert alert-primary">Search a hero</div>
-          <div className="alert alert-danger">
+          <div
+            className="alert alert-primary animate__animated animate__fadeIn"
+            style={{ display: showSearch ? "" : "none" }}
+          >
+            Search a hero
+          </div>
+
+          <div
+            className="alert alert-danger animate__animated animate__fadeIn"
+            style={{ display: showError ? "" : "none" }}
+          >
             No hero with <b>{q}</b>
           </div>
 
-          {/* <HeroCard {..hero} /> */}
+          {heroes.map((hero) => (
+            <HeroCard key={hero.id} {...hero} />
+          ))}
         </div>
       </div>
     </>
