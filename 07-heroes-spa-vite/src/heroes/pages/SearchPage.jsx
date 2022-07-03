95801c5c9ec21ba4484ab7276bd4e7e2f4e4efcd
joseespiritu <joseespiritu89@aragon.unam.mx>
2022-07-02 22:41:20 -0500

    testing library queryParams, forms

--- 07-heroes-spa-vite/src/heroes/pages/SearchPage.jsx
+++ 07-heroes-spa-vite/src/heroes/pages/SearchPage.jsx
@@ -1,14 +1,14 @@
-import { useLocation, useNavigate } from "react-router-dom";
-import queryString from "query-string";
-import { useForm } from "../../hooks/useForm";
-import { HeroCard } from "../components";
-import { getHeroesByName } from "../helpers";
+import { useLocation, useNavigate } from 'react-router-dom';
+import queryString from 'query-string';
+import { useForm } from '../../hooks/useForm';
+import { HeroCard } from '../components';
+import { getHeroesByName } from '../helpers';
 
 export const SearchPage = () => {
   const navigate = useNavigate();
   const location = useLocation();
 
-  const { q = "" } = queryString.parse(location.search);
+  const { q = '' } = queryString.parse(location.search);
   const heroes = getHeroesByName(q);
 
   const showSearch = q.length === 0;
@@ -28,38 +28,39 @@ export const SearchPage = () => {
     <>
       <h1>SearchPage</h1>
       <hr />
-      <div className="row">
-        <div className="col-5">
+      <div className='row'>
+        <div className='col-5'>
           <h4>Searching</h4>
           <hr />
-          <form onSubmit={onSearchSubmit}>
+          <form onSubmit={onSearchSubmit} aria-label='form'>
             <input
-              type="text"
-              placeholder="Search a hero"
-              className="form-control"
-              name="searchText"
-              autoComplete="off"
+              type='text'
+              placeholder='Search a hero'
+              className='form-control'
+              name='searchText'
+              autoComplete='off'
               value={searchText}
               onChange={onInputChange}
             />
-            <button className="btn btn-outline-primary mt-1">Search</button>
+            <button className='btn btn-outline-primary mt-1'>Search</button>
           </form>
         </div>
 
-        <div className="col-7">
+        <div className='col-7'>
           <h4>Results</h4>
           <hr />
 
           <div
-            className="alert alert-primary animate__animated animate__fadeIn"
-            style={{ display: showSearch ? "" : "none" }}
+            className='alert alert-primary animate__animated animate__fadeIn'
+            style={{ display: showSearch ? '' : 'none' }}
           >
             Search a hero
           </div>
 
           <div
-            className="alert alert-danger animate__animated animate__fadeIn"
-            style={{ display: showError ? "" : "none" }}
+            aria-label='alert-danger'
+            className='alert alert-danger animate__animated animate__fadeIn'
+            style={{ display: showError ? '' : 'none' }}
           >
             No hero with <b>{q}</b>
           </div>
