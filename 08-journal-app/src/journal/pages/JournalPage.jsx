eb2af5cd3cefe1db4b1bc444d450cd2503704ab6
joseespiritu <joseespiritu89@aragon.unam.mx>
2022-07-11 21:02:59 -0500

    NothingSelected, Note views

--- 08-journal-app/src/journal/pages/JournalPage.jsx
+++ 08-journal-app/src/journal/pages/JournalPage.jsx
@@ -1,18 +1,20 @@
-import { Typography } from '@mui/material';
 import { JournalLayout } from '../layout/JournalLayout';
+import { NoteView, NothingSelectedView } from '../views';
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography>
+      {/* <Typography>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit eligendi
         quasi recusandae laborum dolorum minus. Deleniti omnis molestias, fugiat
         tempora at maxime dignissimos iusto, doloremque vero cumque nulla ea
         adipisci!
-      </Typography>
+      </Typography> */}
 
       {/* NothingSelected */}
+      {/* <NothingSelectedView /> */}
       {/* NoteView */}
+      <NoteView />
     </JournalLayout>
   );
 };
