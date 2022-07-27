99a4882a8ff435da6f70d2cb21831fb05fe86b95
joseespiritu <joseespiritu89@aragon.unam.mx>
2022-07-26 22:31:12 -0500

    Accion authenticar

--- 08-journal-app/src/store/auth/thunks.js
+++ 08-journal-app/src/store/auth/thunks.js
@@ -1,5 +1,5 @@
 import { signInWithGoogle } from '../../firebase/providers';
-import { checkingCredentials } from './';
+import { checkingCredentials, logout, login } from './';
 
 export const checkingAuthentication = (email, password) => {
   return async (dispatch) => {
@@ -10,6 +10,10 @@ export const checkingAuthentication = (email, password) => {
 export const startGoogleSignIn = () => {
   return async (dispatch) => {
     dispatch(checkingCredentials());
+
     const result = await signInWithGoogle();
+    if (!result.ok) return dispatch(logout(result.errorMessage));
+    // delete result.ok;
+    dispatch(login(result));
   };
 };
