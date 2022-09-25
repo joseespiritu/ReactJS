26f2b261d8ca6913d96b6010bfd598e2eb55d811
joseespiritu <joseespiritu89@aragon.unam.mx>
2022-09-25 12:02:11 -0500

    actualizar nota

--- 08-journal-app/src/journal/views/NoteView.jsx
+++ 08-journal-app/src/journal/views/NoteView.jsx
@@ -1,11 +1,13 @@
+import { useMemo, useEffect } from 'react';
 import { SaveOutlined } from '@mui/icons-material';
 import { Grid, Typography, Button, TextField } from '@mui/material';
-import { useMemo } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useForm } from '../../hooks/useForm';
 import { ImageGallery } from '../components';
+import { setActiveNote, startSaveNote } from '../../store/journal';
 
 export const NoteView = () => {
+  const dispatch = useDispatch();
   const { active: note } = useSelector((state) => state.journal);
 
   const { body, title, date, onInputChange, formState } = useForm(note);
@@ -14,6 +16,14 @@ export const NoteView = () => {
     const newDate = new Date(date);
     return newDate.toUTCString();
   }, [date]);
+
+  useEffect(() => {
+    dispatch(setActiveNote(formState));
+  }, [formState]);
+
+  const onSaveNote = () => {
+    dispatch(startSaveNote());
+  };
 
   return (
     <Grid
@@ -30,7 +40,7 @@ export const NoteView = () => {
         </Typography>
       </Grid>
       <Grid item>
-        <Button color='primary' sx={{ padding: 2 }}>
+        <Button onClick={onSaveNote} color='primary' sx={{ padding: 2 }}>
           <SaveOutlined sx={{ fontSize: 30, mr: 1 }} />
           Guardar
         </Button>
--- 08-journal-app/src/store/journal/journalSlice.js
+++ 08-journal-app/src/store/journal/journalSlice.js
@@ -30,10 +30,19 @@ export const journalSlice = createSlice({
             state.notes = action.payload;
         },
         setSaving: (state) => {
-
+            state.isSaving = true;
+            // TODO: mensaje de error...
         },
-        updateNote: (state, action) => {
+        updateNote: (state, action) => { // payload: note
+            state.isSaving = false;
+            state.notes = state.notes.map(note => {
+                if (note.id === action.payload.id) {
+                    return action.payload;
+                }
+                return note;
+            });
 
+            // TODO: mostrar mensaje de actualizacion
         },
         deleteNoteById: (state, action) => {
 
