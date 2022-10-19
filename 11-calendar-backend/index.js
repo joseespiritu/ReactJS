4bb2bf55806187791f524c313d30c6047305aa76
joseespiritu <joseespiritu89@aragon.unam.mx>
2022-10-18 19:56:53 -0500

    build and deploy with public site backend

--- 11-calendar-backend/index.js
+++ 11-calendar-backend/index.js
@@ -23,6 +23,10 @@ app.use('/health', require('./routes/health'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/events', require('./routes/events'));
 
+app.get('*', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
 // Escuchar peticiones
 app.listen(process.env.PORT, () => {
     console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
