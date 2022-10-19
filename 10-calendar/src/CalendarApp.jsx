7cca9bdf0afe20f296366bee53ad17883bcd258b
joseespiritu <joseespiritu89@aragon.unam.mx>
2022-10-18 19:56:27 -0500

    build and deploy with HashRouter

--- 10-calendar/src/CalendarApp.jsx
+++ 10-calendar/src/CalendarApp.jsx
@@ -1,5 +1,5 @@
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import { AppRouter } from './router/AppRouter';
 import { store } from './store';
 
@@ -7,7 +7,9 @@ export const CalendarApp = () => {
   return (
     <Provider store={store}>
       <BrowserRouter>
+        {/* <HashRouter> */}
         <AppRouter />
+        {/* </HashRouter> */}
       </BrowserRouter>
     </Provider>
   );
