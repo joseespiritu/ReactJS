24b7d7da372878d0a26d39ff837785fdee7538c6
joseespiritu <joseespiritu89@aragon.unam.mx>
2022-10-19 19:50:10 -0500

    cloudinary delete image test

--- 08-journal-app/tests/helpers/fileUpload.test.js
+++ 08-journal-app/tests/helpers/fileUpload.test.js
@@ -1,4 +1,12 @@
+import { v2 as cloudinary } from 'cloudinary';
 import { fileUpload } from "../../src/helpers/fileUpload";
+
+cloudinary.config({
+    cloud_name: '',
+    api_key: '',
+    api_secret: '',
+    secure: true
+});
 
 describe('Pruebas en fileUpload', () => {
     test('debe de subir el archivo correctamente a cloudinary', async () => {
@@ -9,5 +17,21 @@ describe('Pruebas en fileUpload', () => {
 
         const url = await fileUpload(file);
         expect(typeof url).toBe('string');
+
+        // console.log({ url });
+        const segments = url.split('/');
+        const imageId = segments[segments.length - 1].replace('.png', '');
+
+        await cloudinary.api.delete_resources(['journal/' + imageId], {
+            resource_type: 'image'
+        });
+    });
+
+    test('debe de retornar null', async () => {
+        const blob = null;
+        const file = new File([blob], 'foto.jpg');
+
+        const url = await fileUpload(file);
+        expect(url).toBe(null);
     });
 });
