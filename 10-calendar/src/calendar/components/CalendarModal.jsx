68db8befe2c110846b98a162306a7a0f22babfc5
joseespiritu <joseespiritu89@aragon.unam.mx>
2022-10-25 15:51:36 -0500

    testing App Router

--- 10-calendar/src/calendar/components/CalendarModal.jsx
+++ 10-calendar/src/calendar/components/CalendarModal.jsx
@@ -9,6 +9,7 @@ import DatePicker, { registerLocale } from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import es from 'date-fns/locale/es';
 import { useUiStore, useCalendarStore } from '../../hooks';
+import { getEnvVariables } from '../../helpers';
 
 registerLocale('es', es);
 
@@ -23,7 +24,9 @@ const customStyles = {
   },
 };
 
-Modal.setAppElement('#root');
+if (getEnvVariables().VITE_MODE !== 'test') {
+  Modal.setAppElement('#root');
+}
 
 export const CalendarModal = () => {
   const { isDateModalOpen, closeDateModal } = useUiStore();
