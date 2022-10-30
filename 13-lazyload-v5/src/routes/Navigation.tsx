7272f9888d1db5c6432ba0fecd2df99c652a172c
joseespiritu <joseespiritu89@aragon.unam.mx>
2022-10-30 15:59:23 -0600

    init routes lazyload RRD V5

---
+++ 13-lazyload-v5/src/routes/Navigation.tsx
@@ -0,0 +1,46 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink
+} from "react-router-dom";
+import { LazyPage1, LazyPage2, LazyPage3 } from "../01-lazyload/pages";
+
+import logo from "../assets/react.svg";
+
+export const Navigation = () => {
+  return (
+    <Router>
+      <div className="main-layout">
+        <nav>
+            <img src={logo} alt="React Logo" />
+          <ul>
+            <li>
+              <NavLink to="/lazy1" activeClassName="nav-active" exact>Lazy 1</NavLink>
+            </li>
+            <li>
+              <NavLink to="/lazy2" activeClassName="nav-active" exact>Lazy 2</NavLink>
+            </li>
+            <li>
+              <NavLink to="/lazy3" activeClassName="nav-active" exact>Lazy 3</NavLink>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/lazy1">
+            <LazyPage1/>
+          </Route>
+          <Route path="/lazy2">
+            <LazyPage2/>
+          </Route>
+          <Route path="/lazy3">
+            <LazyPage3/>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
