61d67e07767fcfaa67058526be9d73e5a457fb1c
joseespiritu <joseespiritu89@aragon.unam.mx>
2022-11-02 10:50:24 -0600

    Compound component pattern

--- 14-compound-component-pattern/src/02-component-patterns/components/ProductCard.tsx
+++ 14-compound-component-pattern/src/02-component-patterns/components/ProductCard.tsx
@@ -1,3 +1,4 @@
+import { createContext, ReactElement, useContext } from 'react';
 import { useProduct } from '../hooks/useProduct';
 
 import styles from "../styles/styles.module.css";
@@ -5,6 +6,7 @@ import noImage from "../assets/no-image.jpg";
 
 interface Props {
     product: Product;
+    children?: ReactElement | ReactElement[];
 }
 
 interface Product {
@@ -13,22 +15,74 @@ interface Product {
     img?: string;
 }
 
-export const ProductCard = ({ product }: Props) => {
+interface ProductContextProps {
+    counter: number;
+    increaseBy: (value: number) => void;
+    product: Product;
+}
+
+const ProductContext = createContext({} as ProductContextProps);
+const { Provider } = ProductContext;
+
+export const ProductImage = ({ img = '' }) => {
+
+    const { product } = useContext(ProductContext);
+    let imgToShow: string;
+
+    if (img) {
+        imgToShow = img;
+    } else if (product.img) {
+        imgToShow = product.img;
+    } else {
+        imgToShow = noImage;
+    }
+
+    return (
+        <img className={styles.productImg} src={imgToShow} alt="Product Image" />
+    )
+};
+
+export const ProductTitle = ({ title }: { title?: string }) => {
+    const { product } = useContext(ProductContext);
+    return (
+        <span className={styles.productDescription}>{title ? title : product.title}</span>
+    )
+}
+
+export const ProductButtons = () => {
+
+    const { increaseBy, counter } = useContext(ProductContext);
+
+    return (
+        <div className={styles.buttonsContainer}>
+            <button className={styles.buttonMinus} onClick={() => increaseBy(-1)}>-</button>
+            <div className={styles.countLabel}>{counter}</div>
+            <button className={styles.buttonAdd} onClick={() => increaseBy(+1)}>+</button>
+        </div>
+    )
+}
+
+export const ProductCard = ({ children, product }: Props) => {
 
     const { counter, increaseBy } = useProduct();
 
     return (
-        <div className={styles.productCard}>
+        <Provider value={{
+            counter,
+            increaseBy,
+            product
+        }}>
+            <div className={styles.productCard}>
 
-            <img className={styles.productImg} src={product.img ? product.img : noImage} alt="Coffe Mug" />
-
-            <span className={styles.productDescription}>{product.title}</span>
-
-            <div className={styles.buttonsContainer}>
-                <button className={styles.buttonMinus} onClick={() => increaseBy(-1)}>-</button>
-                <div className={styles.countLabel}>{counter}</div>
-                <button className={styles.buttonAdd} onClick={() => increaseBy(+1)}>+</button>
+                {children}
+                {/* <ProductImage img={product.img} />
+                <ProductTitle title={product.title} />
+                <ProductButtons increaseBy={increaseBy} counter={counter} /> */}
             </div>
-        </div>
+        </Provider>
     )
-}
+};
+
+ProductCard.Title = ProductTitle;
+ProductCard.Image = ProductImage;
+ProductCard.Buttons = ProductButtons;
--- 14-compound-component-pattern/src/02-component-patterns/pages/ShoppingPage.tsx
+++ 14-compound-component-pattern/src/02-component-patterns/pages/ShoppingPage.tsx
@@ -1,4 +1,4 @@
-import { ProductCard } from "../components/ProductCard";
+import { ProductButtons, ProductCard, ProductImage, ProductTitle } from '../components/ProductCard';
 
 const product = {
     id: '1',
@@ -17,7 +17,17 @@ export const ShoppingPage = () => {
                 flexDirection: 'row',
                 flexWrap: 'wrap'
             }}>
-                <ProductCard product={product} />
+                <ProductCard product={product}>
+                    <ProductCard.Image />
+                    <ProductCard.Title />
+                    <ProductCard.Buttons />
+                </ProductCard >
+
+                <ProductCard product={product}>
+                    <ProductImage />
+                    <ProductTitle title='Hola Mundo' />
+                    <ProductButtons />
+                </ProductCard >
             </div>
         </div>
     )
